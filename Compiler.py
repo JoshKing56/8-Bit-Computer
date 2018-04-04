a2e8ac9f3e4f04cdcbb2a77f8170fe06b05a6472
Sciencetod0 <joshuaiking56@gmail.com>
2018-04-05 00:14:24 +0100

    Finished LD, organization

--- Compiler.py
+++ Compiler.py
@@ -7,27 +7,50 @@ REGISTERS = ["A", "B", "C", "D"]
 WRITE = ["1000","0100","0010","0001"]
 READ = ["010", "001", "011", "100"] #Order is A, B, C, D
 
-def toHex(binary):
-    unformatted = hex(int(str(binary),2)) #Converts number to base 2, then turns into hex
-    return unformatted[2:len(unformatted)-1] #Chops off the first two characters, '0x'
+#main methods
+def main(): #Main method
+    file = getFileName() #gets file name from args
+    allCommands = openFile(file) #opens the file
 
-def openFile(filename):
+    binaryStrings = [] #creates new array to hold binary values
+    for command in allCommands:
+        binaryStrings.append(returnBinary(command))#populates binaryStrings
+
+    x=0
+    for i in binaryStrings: print(i)
+
+
+    return 0;
+
+def getFileName(): #Returns filename from args. TODO: Should add more checks
+    if (len(sys.argv)!=2):# If the input isn't "python compiler.py [filename]"
+        print("Error 1")
+        return 0;
+    else:
+        return sys.argv[1]
+def openFile(filename): # Opens "filename" as a file
     linearray = []
     sourceFile = open(filename, "r")
     for line in sourceFile:
         linearray.append(line.upper().split()) #Ensures all the lines are upper case, then splits by space
     sourceFile.close()
     return linearray
+def returnBinary(commandLine):
+    opcode = commandLine[0]
+    operands = commandLine[1:len(commandLine)] # rest of the array
+    returnstring = ""
 
-def getFileName(): #Should add more checks
-    if (len(sys.argv)!=2):# If the input isn't "python compiler.py [filename]"
-        print("Error 1")
-        return 0;
+    if (opcode=="LD"):
+        returnstring = ld(operands)
     else:
-        return sys.argv[1]
+        print("Not LD")
 
-def ld(operands):#LD always has two arguments
-    # print(str(operands))
+    return returnstring
+
+
+#Operations
+def ld(operands): #LD
+    # LD always has two arguments
     returnstring = "000 " #LD XX XX
 
     if (operands[0] in REGISTERS): #LD [REGISTER] XX
@@ -44,10 +67,12 @@ def ld(operands):#LD always has two arguments
             returnstring += " 000" #readregister
             returnstring += " 000 " #not used
             returnstring += str(operands[1][1:len(operands[1])]) #RAM address
-
-
-        else: #LD [REGISTER] [CONST]
-            returnstring += "000 " + writeregister + " " + matchReadRegister(operands[1]) + " 000 000000000"
+        else:
+            returnstring += "011 " #subopcode
+            returnstring += writeregister #writeregister
+            returnstring += " 000" #readregister
+            returnstring += " 000 " #not used
+            returnstring += toBinary(operands[1]) #RAM address
 
     elif (operands[0][0:1]=="&"):## LD RAM [REGISTER]
         returnstring += "010 0000 "
@@ -60,45 +85,29 @@ def ld(operands):#LD always has two arguments
 
     return returnstring
 
-def matchWriteRegister(register):
+
+#Support functions
+def toHex(binary): #Converts binary number to Hex
+    unformatted = hex(int(str(binary),2)) #Converts number to base 2, then turns into hex
+    return unformatted[2:len(unformatted)-1] #Chops off the first two characters, '0x'
+def toBinary(decimal): #Converts decimal into binary
+    unformatted = bin(int(decimal))
+    unformatted = unformatted[2:len(unformatted)-1] #Chops off the first two characters, '0b'
+    while (len(unformatted)<8):
+        unformatted = "0" + unformatted
+    return str(unformatted)
+
+def matchWriteRegister(register): #Returns binary for register write
     if (register=="A"): return WRITE[0]
     if (register=="B"): return WRITE[1]
     if (register=="C"): return WRITE[2]
     if (register=="D"): return WRITE[3]
     else: return "Error: 2"
-
-def matchReadRegister(register):
+def matchReadRegister(register): #Returns binary for register read
     if (register=="A"): return READ[0]
     if (register=="B"): return READ[1]
     if (register=="C"): return READ[2]
     if (register=="D"): return READ[3]
     else: return "Error: 3"
-
-def returnBinary(commandLine):
-    opcode = commandLine[0]
-    operands = commandLine[1:len(commandLine)] # rest of the array
-    returnstring = ""
-
-    if (opcode=="LD"):
-        returnstring = ld(operands)
-    else:
-        print("Not LD")
-
-    return returnstring
-
-def main():
-    file = getFileName() #gets file name from args
-    allCommands = openFile(file) #opens the file
-
-    binaryStrings = [] #creates new array to hold binary values
-    for command in allCommands:
-        binaryStrings.append(returnBinary(command))#populates binaryStrings
-
-    x=0
-    for i in binaryStrings: print(i)
-
-
-    return 0;
-
 
 main()
