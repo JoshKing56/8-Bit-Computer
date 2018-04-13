c42fd09f9439c2d46dc8cf56bfe0e4105e720d87
Sciencetod0 <joshuaiking56@gmail.com>
2018-04-13 18:58:14 +0100

    Finished Bubblesort!

--- sorting.asm
+++ sorting.asm
@@ -1,50 +1,44 @@
 CLR
-LD A 6      ;;Init Load array {6, 99, 36, 66, 24, 30, 46, 26, 86, 54, 75}
+LD A 15      ;;Init Load array {15,63,7,255,1,127,3,31}
 LD &0 A
-LD A 99
+LD A 63
 LD &1 A
-LD A 36
+LD A 7
 LD &2 A
-LD A 66
+LD A 255
 LD &3 A
-LD A 24
+LD A 1
 LD &4 A
-LD A 30
+LD A 127
 LD &5 A
-LD A 46
+LD A 3
 LD &6 A
-LD A 26
-LD &7 A
-LD A 86
-LD &8 A
-LD A 54
-LD &9 A
-LD A 75
-LD &10 A        ;;Done loading
-LD C 0
-LD D 1
+LD A 31
+LD &7 A         ;;Done loading
 LD A 0          ;;NotSorted
 LD &15 A
+LD C 0
+LD D 1
 LD A &C         ;;NextPair
 LD B &D
 GR A B A
-PCU 0      ;;Jump to NoSwap
-JNZ A 38    ;;Jump to NoSwap
+PCU 0           ;;Jump to NoSwap
+JNZ A 32        ;;Jump to NoSwap
 LD A &D
 LD B &C
 LD &C A
 LD &D B
 LD A 1
 LD &15 A
-EQ A D 10       ;;NoSwap
+EQ A D 7        ;;NoSwap
 PCU 0           ;;Jump to EOArray
-JNZ A 45        ;;Jump to EOArray
+JNZ A 39        ;;Jump to EOArray
 LD C D
 ADD D D 1
 PCU 0           ;;Jump to NextPair
-PCL 27          ;;Jump to NextPair
+PCL 21          ;;Jump to NextPair
 LD A &15        ;;EOArray
 PCU 0           ;;Jump Not Sorted
-JNZ A 25        ;;Jump Not Sorted
+JNZ A 17        ;;Jump Not Sorted
 PCU 0
-PCL 49 ;;Jump to self
+PCL 43 ;;Jump to self
