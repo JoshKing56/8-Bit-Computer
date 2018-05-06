3216bb75ea9164a2959fed177f80d47172003522
Chris Hill <chris.j.hill94@gmail.com>
2018-05-06 17:05:39 +0100

    due speed test code

---
+++ Arduino Code/main_due/main_due.ino
@@ -0,0 +1,30 @@
+#include <DueTimer.h>
+
+#define pinnum 13
+#define pinport PIOB
+#define pinmask (1<<27)   // Arduino pin 2 is port B pin 25.
+
+volatile byte flag = 0;
+
+bool ledOn = false;
+void myHandler() {
+  if (flag == 1) {
+    pinport -> PIO_SODR = pinmask ;   // set pin
+    flag = 0;
+  }
+  else {
+    pinport -> PIO_CODR = pinmask ;  // clear pin
+    flag = 1;
+  }
+}
+
+void setup() {
+  pinMode (pinnum, OUTPUT) ;
+
+  Timer3.attachInterrupt(myHandler);
+  Timer3.start(1); // Calls every 50ms
+}
+
+void loop() {
+
+}
